fb3769099372223d6103388c39453f92b72b1d63
zhenia-chugaev <zhenia.chugaev@bk.ru>
2024-06-23 15:25:05 +0300

    add login form markup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,22 @@
-const Login = () => <p>Login form</p>;
+import { Formik, Form, Field } from 'formik';
+
+const Login = () => {
+  const initialValues = {
+    username: '',
+    password: '',
+  };
+
+  const onSubmit = (values) => console.log(values);
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Form>
+        <Field type="text" name="username" required />
+        <Field type="password" name="password" required />
+        <button type="submit">Войти</button>
+      </Form>
+    </Formik>
+  );
+};
 
 export default Login;
