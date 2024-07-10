6c3af693763955733a06c9734884e1f9a42d1e7c
zhenia-chugaev <zhenia.chugaev@bk.ru>
2024-07-10 21:26:50 +0300

    add application layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import { Main, Login, NotFound } from './pages';
 
 const App = () => (
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="login" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div className="d-flex flex-column vh-100">
+      <header className="z-1 shadow-sm">
+        <Container>
+          <Navbar>
+            <Navbar.Brand as={Link} to="/">Slack</Navbar.Brand>
+          </Navbar>
+        </Container>
+      </header>
+      <main className="flex-grow-1 bg-light">
+        <Container className="h-100">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Container>
+      </main>
+    </div>
   </BrowserRouter>
 );
 
