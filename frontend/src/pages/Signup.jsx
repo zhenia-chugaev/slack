bb4a41e1e5186bceace93f4fd44995bb2ca7ec1f
zhenia-chugaev <zhenia.chugaev@bk.ru>
2024-08-10 16:20:47 +0300

    add signup page

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,21 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+const Signup = () => (
+  <Row className="h-100 justify-content-center align-content-center">
+    <Col as="section" className="border border-secondary-subtle rounded-2 shadow-sm bg-white" lg={6}>
+      <Row className="gap-3 align-items-center">
+        <Col className="p-5 pe-0" lg={5}>
+          <Image src="/assets/logo.png" roundedCircle fluid />
+        </Col>
+        <Col className="p-5">
+          <h2 className="mb-3 h1 text-center">Регистрация</h2>
+          <div>Signup form</div>
+        </Col>
+      </Row>
+    </Col>
+  </Row>
+);
+
+export default Signup;
