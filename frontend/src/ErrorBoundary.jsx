41b64a87558431b3354ed2859b050467b87deaee
zhenia-chugaev <zhenia.chugaev@bk.ru>
2024-09-07 16:05:57 +0300

    set up error reporting

--- frontend/src/ErrorBoundary.jsx
+++ frontend/src/ErrorBoundary.jsx
@@ -1,38 +1,27 @@
-import React from 'react';
-import { withTranslation } from 'react-i18next';
+import { ErrorBoundary as RollbarErrorBoundary } from '@rollbar/react';
+import { useTranslation } from 'react-i18next';
 import Button from 'react-bootstrap/Button';
 import { EmojiDizzy } from 'react-bootstrap-icons';
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
+const FallbackUI = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center h-100">
+      <EmojiDizzy className="mb-2 text-danger" size={55} />
+      <p className="fs-4 fw-light">
+        {t('warnings.general')}
+      </p>
+      <Button href="/" variant="outline-secondary">
+        {t('buttons.goHome')}
+      </Button>
+    </div>
+  );
+};
 
-  static getDerivedStateFromError() {
-    return { hasError: true };
-  }
+const ErrorBoundary = ({ children }) => (
+  <RollbarErrorBoundary fallbackUI={FallbackUI}>
+    {children}
+  </RollbarErrorBoundary>
+);
 
-  static getFallbackUi(t) {
-    return (
-      <div className="d-flex flex-column align-items-center justify-content-center h-100">
-        <EmojiDizzy className="mb-2 text-danger" size={55} />
-        <p className="fs-4 fw-light">
-          {t('warnings.general')}
-        </p>
-        <Button href="/" variant="outline-secondary">
-          {t('buttons.goHome')}
-        </Button>
-      </div>
-    );
-  }
-
-  render() {
-    const { children, t } = this.props;
-    const { hasError } = this.state;
-    const { getFallbackUi } = this.constructor;
-    return hasError ? getFallbackUi(t) : children;
-  }
-}
-
-export default withTranslation()(ErrorBoundary);
+export default ErrorBoundary;
